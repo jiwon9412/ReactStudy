573f6e2ba584317ebbae4c65fc987176d67354f4
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2022-06-17 23:23:36 +0900

    [BUG] ant design 적용 중 버그 수정

--- src/components/pages/hooks/index.js
+++ src/components/pages/hooks/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useMemo, useCallback } from 'react';
 import { Button, message } from 'antd';
 import { Link } from 'react-router-dom';
-import Main from '../main';
 
 const Hooks = () => {
   /**사용자 정보 */
