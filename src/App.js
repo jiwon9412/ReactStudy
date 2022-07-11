eadc5b3fcbfdc7f070cb74c7ad1bf9e2d4e66416
jiwon9412 <80965224+jiwon9412@users.noreply.github.com>
2022-07-11 21:53:40 +0900

    INIT: clone Dribbble site

--- src/App.js
+++ src/App.js
@@ -1,60 +1,110 @@
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Row, Col, Spin } from 'antd';
+import { Row, Col, Spin, Layout } from 'antd';
 import { useSelector } from 'react-redux';
+import styled from 'styled-components';
+import { SearchOutlined } from '@ant-design/icons';
+import Logo from './assets/temp/temp/logo.svg';
+import menuList from './assets/temp/temp/menu.json';
 
-import Main from './components/pages/main';
-import Login from './components/pages/login';
-import Join from './components/pages/join';
-import TableTest from './components/pages/table';
-import Hooks from './components/pages/hooks';
-import Board from './components/pages/board';
-import BoardDetail from './components/pages/board/Detail';
-import BoardUpdate from './components/pages/board/Update';
-import BoardInsert from './components/pages/board/Insert';
-
-import Header from './components/templates/Header';
-import Footer from './components/templates/Footer';
-
-function App() {
-  const { isLoading } = useSelector((state) => state.common);
-
+export default function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Spin spinning={isLoading}>
+    <BrowserRouter>
+      <Layout>
+        <DribbbleHeader>
           <Row>
-            <Col span={24}>
+            <Col>
               <Row>
-                <Col span={24}>
-                  <Header />
+                <Col>
+                  <a href="/#">
+                    <img width={92} src={Logo} />
+                  </a>
                 </Col>
-              </Row>
-              <Row>
-                <Col span={24}>
-                  <Routes>
-                    <Route path="/main" element={<Main />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/join" element={<Join />} />
-                    <Route path="/table" element={<TableTest />} />
-                    <Route path="/hooks" element={<Hooks />} />
-                    <Route path="/board" element={<Board />} />
-                    <Route path="/board/detail/:id" element={<BoardDetail />} />
-                    <Route path="/board/update/:id" element={<BoardUpdate />} />
-                    <Route path="/board/insert" element={<BoardInsert />} />
-                  </Routes>
-                </Col>
-              </Row>
-              <Row>
-                <Col span={24}>
-                  <Footer />
+                <Col>
+                  <ul className="menuList">
+                    {menuList.map((menu) => {
+                      return (
+                        <Menu>
+                          <a href="/#" className="menu">
+                            {menu.menuNm}
+                          </a>
+                        </Menu>
+                      );
+                    })}
+                  </ul>
                 </Col>
               </Row>
             </Col>
+            <Col style={{ marginLeft: 'auto' }}>
+              <ul>
+                <Action right={10}>
+                  <a href="/#">
+                    <SearchOutlined
+                      style={{ fontSize: 18, color: '#6e6d7a' }}
+                    />
+                  </a>
+                </Action>
+                <Action left={10} right={10}>
+                  <a href="/#" className="signIn">
+                    Sign in
+                  </a>
+                </Action>
+                <Action left={10}>
+                  <a href="/#" className="btnAction">
+                    Share my work
+                  </a>
+                </Action>
+              </ul>
+            </Col>
           </Row>
-        </Spin>
-      </BrowserRouter>
-    </div>
+        </DribbbleHeader>
+        <Content>컨텐츠</Content>
+        <Footer>푸터</Footer>
+      </Layout>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const { Header, Footer, Sider, Content } = Layout;
+
+const DribbbleHeader = styled(Header)`
+  background: #fff;
+  height: 80px;
+  box-shadow: inset 0px -1px 0px #f3f3f4;
+  line-height: 80px;
+  padding: 0 15px;
+
+  .menuList {
+    padding-left: 0;
+  }
+
+  .signIn {
+    font-size: 16px;
+    color: #6e6d7a;
+  }
+
+  .btnAction {
+    padding: 10px 16px;
+    border-radius: 8px;
+    background: #ea4c89;
+    color: #fff;
+    font-weight: 500;
+  }
+`;
+
+const Menu = styled.li`
+  display: inline;
+  padding: 30px 16px;
+  .menu {
+    font-size: 14px;
+    color: #6e6d7a;
+    font-weight: 500;
+  }
+`;
+
+//동적 컴포넌트!!!!!!!!!!!
+const Action = styled.li`
+  display: inline;
+  margin-left: ${(props) => props.left || 0}px;
+  margin-right: ${(props) => props.right || 0}px;
+`;
